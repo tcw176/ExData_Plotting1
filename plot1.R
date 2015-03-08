0d7f095d13f021222144019cebd212ee43377577
tcw176 <tcw176@users.noreply.github.com>
2015-03-08 22:54:08 +0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -6,10 +6,12 @@ data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", 
 data$Date <- dmy(data$Date)
 data2 <- filter(data, data$Date == ymd("2007-02-01") | data$Date == ymd("2007-02-02"))
 
+png(file = "plot1.png")
+
 hist(data2$Global_active_power, col = "red", main = "",  xlab = "")
 
-title(main = "Global Active Power", font.main = 1, cex.main = 0.85, cex.lab = 0.75,
-      cex.sub = 0.75, xlab = "Global Active Power (kilowattts)")
+title(main = "Global Active Power", font.main = 1, cex.main = 1, cex.lab = 1,
+      cex.sub = 1, xlab = "Global Active Power (kilowattts)")
 
-dev.copy(png, file = "plot1.png")
+#dev.copy(png, file = "plot1.png")
 dev.off()
