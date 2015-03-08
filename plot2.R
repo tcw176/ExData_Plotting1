f62cc765809c27aa77b38c16293f3594178bbafb
tcw176 <tcw176@users.noreply.github.com>
2015-03-08 17:58:34 +0800

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,8 @@
+library(lubridate)
+data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
+data$Date <- dmy(data$Date)
+data2 <- filter(data, data$Date == ymd("2007-02-01") | data$Date == ymd("2007-02-02"))
+data2 <- mutate(data2, DateTime = ymd(Date) + hms(Time))
+plot(data2$DateTime, data2$Global_active_power, cex.lab = 0.75, cex.axis = 0.75, ylab = "Global Active Power (kilowatts)", type = "l", xlab = "")
+dev.copy(png, file = "plot2.png")
+dev.off()
