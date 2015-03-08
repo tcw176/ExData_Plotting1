110e030ef5799a354498d3eff12ca76462c704c9
tcw176 <tcw176@users.noreply.github.com>
2015-03-08 22:56:09 +0800

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -7,33 +7,35 @@ data$Date <- dmy(data$Date)
 data2 <- filter(data, data$Date == ymd("2007-02-01") | data$Date == ymd("2007-02-02"))
 data2 <- mutate(data2, DateTime = ymd(Date) + hms(Time))
 
+png(file = "plot4.png")
+
 par(mfrow = c(2,2))
 
-plot(data2$DateTime, data2$Global_active_power, cex.lab = 0.7, cex.axis = 0.7, ylab = "Global Active Power", type = "l", xlab = "")
+plot(data2$DateTime, data2$Global_active_power, cex.lab = 1 , cex.axis = 1, 
+     ylab = "Global Active Power", type = "l", xlab = "")
 
 plot(data2$DateTime, data2$Voltage, type = "l", xlab = "datetime", ylab = "Voltage", 
-     cex.axis = 0.75, cex.lab = 0.75)
+     cex.axis = 1, cex.lab = 1)
 
 
 plot(data2$DateTime, data2$Sub_metering_1, type = "l", ylim = c(0,40),
-     cex.axis = 0.75, cex.lab = 0.75,
+     cex.axis = 1, cex.lab = 1,
      xlab = "", ylab = "Energy Sub metering")
 par(new=T)
 plot(data2$DateTime, data2$Sub_metering_2, type = "l", ylim = c(0,40),
-     cex.axis = 0.75, cex.lab = 0.75,
+     cex.axis = 1, cex.lab = 1,
      col = "red", xlab = "", ylab = "")
 par(new=T)
 plot(data2$DateTime, data2$Sub_metering_3, type = "l", ylim = c(0,40),
-     cex.axis = 0.75, cex.lab = 0.75,
+     cex.axis = 1, cex.lab = 1,
      col = "blue", xlab = "", ylab = "")
 
-legend("topright", legend = c("Sub_metering_1                  ", "Sub_metering_2              ",                      
-      "Sub_metering_3             "), 
-      y.intersp = 1.3, cex = 0.75, col = c("black", "red", "blue"), 
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2",                      
+      "Sub_metering_3"), y.intersp = 1.3, cex = 0.75, col = c("black", "red", "blue"), 
       bty = "n", lty = c(1, 1, 1))
 
 plot(data2$DateTime, data2$Global_reactive_power, type = "l", xlab = "datetime",
-     ylab = "Global_reactive_power", cex.axis = 0.6, cex.lab = 0.75)
+     ylab = "Global_reactive_power", cex.axis = 0.6, cex.lab = 1)
 
-dev.copy(png, file = "plot4.png")
+#dev.copy(png, file = "plot4.png")
 dev.off()
