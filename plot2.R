3f9f9013a4c0859852f58bf96e876e60be0decd3
tcw176 <tcw176@users.noreply.github.com>
2015-03-08 22:54:44 +0800

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -7,8 +7,9 @@ data$Date <- dmy(data$Date)
 data2 <- filter(data, data$Date == ymd("2007-02-01") | data$Date == ymd("2007-02-02"))
 data2 <- mutate(data2, DateTime = ymd(Date) + hms(Time))
 
-plot(data2$DateTime, data2$Global_active_power, cex.lab = 0.75, cex.axis = 0.75, ylab = "Global Active Power (kilowatts)", 
-        type = "l", xlab = "")
+png(file = "plot2.png")
 
-dev.copy(png, file = "plot2.png")
+plot(data2$DateTime, data2$Global_active_power, cex.lab = 1, cex.axis = 1, 
+     ylab = "Global Active Power (kilowatts)", type = "l", xlab = "")
+
 dev.off()
