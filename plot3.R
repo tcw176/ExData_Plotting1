5f80d221b3b6e8795ac6698776bb70caf1b08df2
tcw176 <tcw176@users.noreply.github.com>
2015-03-08 21:14:30 +0800

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,22 @@
+library(lubridate)
+data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
+data$Date <- dmy(data$Date)
+data2 <- filter(data, data$Date == ymd("2007-02-01") | data$Date == ymd("2007-02-02"))
+data2 <- mutate(data2, DateTime = ymd(Date) + hms(Time))
+
+plot(data2$DateTime, data2$Sub_metering_1, type = "l", ylim = c(0,40),
+     cex.axis = 0.75, cex.lab = 0.75,
+     xlab = "", ylab = "Energy Sub metering")
+par(new=T)
+plot(data2$DateTime, data2$Sub_metering_2, type = "l", ylim = c(0,40),
+     cex.axis = 0.75, cex.lab = 0.75,
+     col = "red", xlab = "", ylab = "")
+par(new=T)
+plot(data2$DateTime, data2$Sub_metering_3, type = "l", ylim = c(0,40),
+     cex.axis = 0.75, cex.lab = 0.75,
+     col = "blue", xlab = "", ylab = "")
+
+legend("topright", legend = c("Sub_metering_1  ", "Sub_metering_2  ", 
+      "Sub_metering_3  "), cex = 0.7, col = c("black", "red", "blue"), lty = c(1, 1, 1))
+dev.copy(png, file = "plot3.png")
+dev.off()
